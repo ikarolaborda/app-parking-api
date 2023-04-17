f273712351709fd0983c00a4621871d9963c4c76
Ikaro C. Laborda <iclaborda@msn.com>
2023-04-16 22:36:40 -0300

    feat: implementing additional calculation for parking prices

--- app/Services/ParkingService.php
+++ app/Services/ParkingService.php
@@ -40,5 +40,17 @@ class ParkingService
         return $this->parkingRepository->getStoppedParkings();
     }
 
+    public static function calculatePrice(int $zone_id, string $startTime, string $stopTime = null): int
+    {
+        $start = new Carbon($startTime);
+        $stop = (!is_null($stopTime)) ? new Carbon($stopTime) : now();
+
+        $totalTimeByMinutes = $stop->diffInMinutes($start);
+
+        $priceByMinutes = Zone::find($zone_id)->price_per_hour / 60;
+
+        return ceil($totalTimeByMinutes * $priceByMinutes);
+    }
+
 
 }
